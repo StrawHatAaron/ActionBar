e9b64c41c533074f8f8cd607a7081bf2d9812397
aaronmiller <aaronmiller822028@gmail.com>
2017-07-25 16:50:18 -0700

    Initial commit

---
+++ app/src/main/java/com/aaronjeromemiller/actionbar/BlogFragment.java
@@ -0,0 +1,36 @@
+package com.aaronjeromemiller.actionbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by aaronmiller on 7/25/17.
+ */
+
+public class BlogFragment extends Fragment{
+    private static final String TAG = "Tab1Fragment";
+
+    private Button btnTEST;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.blog_fragment,container,false);
+        btnTEST = (Button) view.findViewById(R.id.btnTEST);
+
+        btnTEST.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
